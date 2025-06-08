b6bd484c6ae98270e286735a7a40644d0ff4063c
surya-srib <70128828+surya-srib@users.noreply.github.com>
2025-06-08 23:44:55 +0530

    Update coco_eval.py

--- rfdetr/datasets/coco_eval.py
+++ rfdetr/datasets/coco_eval.py
@@ -30,7 +30,7 @@ from pycocotools.cocoeval import COCOeval
 from pycocotools.coco import COCO
 import pycocotools.mask as mask_util
 
-from rfdetr.util.misc import all_gather
+from util.misc import all_gather
 
 
 class CocoEvaluator(object):
