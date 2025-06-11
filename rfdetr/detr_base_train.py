bfb24cb9be58c4aee789360c089a4dde7a329110
surya-srib <70128828+surya-srib@users.noreply.github.com>
2025-06-11 11:58:47 +0530

    Create detr_base_train.py

---
+++ rfdetr/detr_base_train.py
@@ -0,0 +1,29 @@
+from detr import RFDETRBase
+import torch
+import argparse
+
+path = "<path>"
+epochs_count = 12
+
+parser = argparse.ArgumentParser("Detr Base train parser")
+parser.add_argument('--path', type=str)
+parser.add_argument('--epochs', type=int)
+
+args = parser.parse_args()
+
+if args.path:
+    path = args.path
+
+if args.epochs:
+    epochs_count = args.epochs
+
+print(path)
+print(epochs_count)
+
+if __name__ == "__main__":
+    model = RFDETRBase()
+    model.train(
+        dataset_dir=path,
+        epochs=epochs_count,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    )
